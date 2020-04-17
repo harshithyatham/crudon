da01928b760122b020cc6328e31b11367d6069e6
Harshith yatham <Harshith yatham@LAPTOP-VH7N87B0.mshome.net>
2020-04-17 16:10:52 +0530

    fourth commit

--- Gstproduct-application/src/main/java/com/jango/demo/controller/productcontroller.java
+++ Gstproduct-application/src/main/java/com/jango/demo/controller/productcontroller.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +22,7 @@ import com.jango.demo.model.product;
 public class productcontroller {
 	@Autowired
 	private productdao dao;
-	
+	@CrossOrigin(origins="http://localhost:4200")
 	@RequestMapping(method=RequestMethod.POST,value="/pdt")
 	 public String newproduct(@RequestBody product pro)
 	 {
